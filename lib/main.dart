5f6f95f4089f0c554ff7aeb39efe7cb51556f13f
chien410 <82858779+chien410@users.noreply.github.com>
2021-04-21 14:20:45 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'home/Home.dart';
 import 'login/login_page.dart';
 import 'member/Member.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -16,57 +15,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        bottomAppBarColor: Colors.brown,
-        buttonTheme:ButtonThemeData(minWidth: double.infinity,height:50)),
+          primarySwatch: Colors.yellow,
+          bottomAppBarColor: Colors.brown,
+          buttonTheme: ButtonThemeData(minWidth: double.infinity, height: 50)),
 
-      home: MyHomePage(),
+      home: LoginPage(),
+
+      // Navigator.pushNamed 使用前的預先設定
+      initialRoute: '/',
+      routes: {
+        "/l": (context) => LoginPage(),
+        "/home": (context) => Home(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int count = 0;
+
   //navbar點擊的頁面
   int pageIndex = 0;
+
   final pages = [
     Home(),
     CourseInfo(),
-    Member(),
-    LoginPage(),
+    Member()
+    // LoginPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body:pages[pageIndex],
+      body: pages[pageIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items:[
-          BottomNavigationBarItem(icon:Icon(Icons.home),label:'首頁'),
-          BottomNavigationBarItem(icon:Icon(Icons.list),label:'課程'),
-          BottomNavigationBarItem(icon:Icon(Icons.account_circle),label:'會員中心'),
-          BottomNavigationBarItem(icon:Icon(Icons.account_circle_rounded),label:'登入'),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首頁'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: '課程'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: '會員中心')
+          // BottomNavigationBarItem(
+          //     icon: Icon(Icons.account_circle_rounded), label: '登入'),
         ],
-       backgroundColor: Colors.grey[800],
-       unselectedItemColor: Colors.white,
-       fixedColor: Colors.amber,
-       currentIndex: pageIndex,
-        onTap:(index){
+        backgroundColor: Colors.grey[800],
+        unselectedItemColor: Colors.white,
+        fixedColor: Colors.amber,
+        currentIndex: pageIndex,
+        onTap: (index) {
           setState(() {
             pageIndex = index;
           });
-         },
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.adb),
-        onPressed: (){
+        onPressed: () {
           // setState()：當需要畫面更新時，就會呼叫此函數，並觸發 build()進重建 Scaffold
           setState(() {
             count += 1;
--- lib/member/Member.dart
+++ lib/member/Member.dart
@@ -1,49 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/login/login_page.dart';
+
 class Member extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('登入'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.login_outlined,
-                color: Colors.blueGrey,
-              ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                    builder: (_) => LoginPage())); // do something
-              },
-            )
-          ],
-
+      title: Text('登入'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.login_outlined,
+            color: Colors.blueGrey,
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (_) => LoginPage())); // do something
+          },
         )
+      ],
+    ));
 
-    );
-
-      //     body:SingleChildScrollView(
-      //       child:Column(
-      //         children:<Widget>[
-      //
-      //           Container(
-      //             width:MediaQuery.of(context).size.width,
-      //             child: Padding(
-      //               padding: const EdgeInsets.all(16.0),
-      //               child:ElevatedButton(
-      //                 child:Text('登入',style: TextStyle(fontSize: 20),),
-      //                 onPressed: (){
-      //
-      //                       Navigator.push(context, MaterialPageRoute(builder: (_) => LoginPage()));
-      //                },
-      //               ),
-      //             ),
-      //           )
-      //         ],
-      //       ),
-      //     )
-      // );
-
+    //     body:SingleChildScrollView(
+    //       child:Column(
+    //         children:<Widget>[
+    //
+    //           Container(
+    //             width:MediaQuery.of(context).size.width,
+    //             child: Padding(
+    //               padding: const EdgeInsets.all(16.0),
+    //               child:ElevatedButton(
+    //                 child:Text('登入',style: TextStyle(fontSize: 20),),
+    //                 onPressed: (){
+    //
+    //                       Navigator.push(context, MaterialPageRoute(builder: (_) => LoginPage()));
+    //                },
+    //               ),
+    //             ),
+    //           )
+    //         ],
+    //       ),
+    //     )
+    // );
   }
 }
